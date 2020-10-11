401b98ef676d648f852ab6d808f004f1ea1b2802
Alberto Martin <albermm.78@gmail.com>
2020-10-11 16:06:27 +1300

    create form added, crispy forms added

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django import forms
 from django.urls import reverse
+from crispy_forms.helper import FormHelper
 
 from . import util
 
@@ -9,36 +10,20 @@ class NewSearchForm(forms.Form):
     query = forms.CharField(label=False, widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))    
 
 class NewPageForm(forms.Form):
-    query = forms.CharField(label=False, widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))    
-
+    title = forms.CharField(label= 'Title')
+    textarea = forms.CharField(help_text= ' write encyclopedia content', widget=forms.Textarea(attrs={"rows":10}))
+    helper = FormHelper()
 
 # Create your views here.
-'''
-def index(request):
-    if request.method == "POST":
-        form = NewSearchForm(request.POST)
-        if form.is_valid():
-            query = form.cleaned_data["query"]
-            return title(request, query)
-        else:
-            return render(request, "encyclopedia/index.html", {
-                "form" : form
-            })
-           
-    
-    return render(request, "encyclopedia/index.html", {
-             "entries": util.list_entries(),
-             "form" : NewSearchForm(auto_id=False)
-    })
-'''
-#request.session["pages"]
 
 def index(request):
+    '''
     if "pages" not in request.session:
         request.session["pages"] = []
         return render(request, "encyclopedia/index.html", {
             "pages": request.session["pages"]
         })
+    '''        
     query = request.GET.get('q')
     if query:
         title, content, result = search(request, query)['query'], search(request, query)['content'], search(request, query)['result']        
@@ -65,9 +50,7 @@ def title(request, title):
 
 def search(request, query):
     lista = util.list_entries()
-    print ("query: ", query)
-    print ("lista: ", lista)
-    result = next((s for s in lista if query in s), None)
+    result = next((s for s in lista if query in s)) 
     content = util.get_entry(query)
     if query in lista:
         resultado = {"query": query, "content":content, "result":""}
@@ -82,10 +65,14 @@ def add(request):
     if request.method == "POST":
         form = NewPageForm(request.POST)
         if form.is_valid():
+            page = form.save()#(commit=False)
             page = form.cleaned_data["page"]
             request.session["pages"] += [page]
-            return HttpResponseRedirect(reverse("wiki:index"))
+            page.save() 
+            return redirect('page_detail', id=page.id)
+            #return HttpResponseRedirect(reverse("wiki:index"))
         else:
+            form = NewPageForm()
             return render(request, "encyclopedia/add.html", {
                 "form": form
             })
