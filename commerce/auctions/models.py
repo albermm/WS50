a562339cf2dcbce6e80a65428230edec1178bd5a
Alberto Martin <albermm.78@gmail.com>
2020-10-16 11:30:12 +1300

    primeros pasos, modelo, vistas y url

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+#from . import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Listings(models.Model):
+    listing_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    startingbid = models.IntegerField()
+    image = models.CharField(max_length=500)
+    category = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return f"{self.listing_id}: {self.title}, description: {self.description}, initial bid: {self.startingbid}, category: {self.category}"
+    
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,12 +3,16 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from .models import Listings
+
 
 from .models import User
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "listings": Listings.objects.all()
+    })
 
 
 def login_view(request):
@@ -61,3 +65,10 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create(request):
+    pass
+
+def listing(request):
+    pass
